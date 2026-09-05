329efc75e9397b8ca52124989c8f7b5539d0cf0c
hbnf <324434578+gnurthbnf@users.noreply.github.com>
2026-09-05 21:17:10 +0700

    Update learn01-python.py

--- Python/learn01-python.py
+++ Python/learn01-python.py
@@ -1,3 +1,3 @@
 from identify_passkey import identify_passkey, PasskeyIcon, PasskeyDescriptor
 
-passkey_info = indentify_passkey ("da1fa263-8b25-42b6-a820-c0036f21ba7f") #ATKey.Card NFC
+passkey_info = identify_passkey ("da1fa263-8b25-42b6-a820-c0036f21ba7f") #ATKey.Card NFC
